f3d4e7339a9137fadd28d46aa57ec09b917ad29a
Nicholas Fordyce <FordyceNick@gmail.com>
2017-05-23 23:01:36 -0400

    Add Add Thing button functionality

--- src/AddButton.js
+++ src/AddButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './AddButton.css'
 
 const AddButton = (props) => {
-    return <button className="add-thing">Add Thing</button>
+    return <button className="add-thing" onClick={props.click}>Add Thing</button>
 }
 
 export default AddButton
--- src/App.js
+++ src/App.js
@@ -6,19 +6,42 @@ import ThingList from './ThingList'
 import AddButton from './AddButton'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.addThing = this.addThing.bind(this)
+  }
+
   state = {
     things: {
       'thing-1': { id: 'thing-1', name: 'Milk'},
       'thing-2': { id: 'thing-2', name: 'Bread'},
       'thing-3': { id: 'thing-3', name: 'Bibb lettuce'}
+    },
+    thingCount: 4
+  }
+
+  addThing(e){
+    let thingsObj = {...this.state.things}
+    let name = document.querySelector('.thingField').value
+    thingsObj[`thing-${this.state.thingCount}`] = { 
+      id: `thing-${this.state.thingCount}`, name: name
     }
+
+    this.setState({
+      things: thingsObj,
+      thingCount: ++this.state.thingCount
+    },
+      () => console.log(this.state))
   }
 
   render() {
     return (
       <div className="App">
         <Header />
-        <AddButton />
+        <div className="fieldDiv">
+          <input className="thingField" type="text" placeholder="Write thing here"/>
+        </div>
+        <AddButton click={this.addThing}/>
         <ThingList things={this.state.things}/>
       </div>
     );
